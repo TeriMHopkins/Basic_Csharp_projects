0933f8a0b39fa64e7dfd259b06d113ca15b9d1a9
TeriMHopkins <TeriMHopkins@users.noreply.github.com>
2021-01-10 15:54:21 -0800

    commit and push string project

---
+++ stringProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace stringProject
+{
+    class Program
+    {
+        static void Main()
+        {
+            string fname = "Teri ";
+            string mname = "Michelle ";
+            string lname = "Carris";
+            string fullname = fname + mname + lname;
+            Console.WriteLine(fullname.ToUpper());
+            Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("Hello! ");
+            sb.Append("This is my string builder paragraph. ");
+            sb.Append("The end.");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
